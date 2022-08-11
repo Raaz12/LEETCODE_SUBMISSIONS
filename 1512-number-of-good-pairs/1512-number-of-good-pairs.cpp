c1a9ef35eb7754fb2f1d0a5dfdfec08e292faccc
Raaz12 <66207819+Raaz12@users.noreply.github.com>
2022-08-12 00:18:48 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (60.40%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int sum(int n)
-    {
-        int sum=0;
-        for(int i=1;i<=n;i++)
-        {
-            sum+=i;
-        }
-        return sum;
-    }
+    // int sum(int n)
+    // {
+    //     int sum=0;
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         sum+=i;
+    //     }
+    //     return sum;
+    // }
     int numIdenticalPairs(vector<int>& nums) {
         int n=nums.size();
         int count=0;
@@ -19,7 +19,8 @@ public:
         }
         for(auto val:mp)
         {
-            count+=sum(val.second-1);
+            int sum=val.second;
+            count+=(sum*(sum-1))/2;
         }
         return count;
     }
