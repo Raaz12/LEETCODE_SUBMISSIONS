fe0be495c5a4165a655000d1ad8fa853141b5d71
Raaz12 <66207819+Raaz12@users.noreply.github.com>
2022-08-14 02:35:16 +0530

    Time: 10 ms (7.69%), Space: 6.1 MB (53.37%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if(n==1563712132)   return true;
+        // if(n==1563712132)   return true;
        int mp[100001]={0};
         int ans=n;
         while(ans!=1)
@@ -11,12 +11,12 @@ public:
            {
                ans+=((n%10)*(n%10));
                 n/=10;
-               mp[ans]++;
+               // mp[ans]++;
            }
             n=ans;
             if(ans==1)  return true;
-            if(mp[ans]>1)  return false;
-            
+            if(mp[ans]==1)  return false;
+            mp[ans]=1;
         }
         return true;
     }
