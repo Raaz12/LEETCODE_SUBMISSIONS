1b6c219a2c394a1e65e79931e509718c21cd89e6
Raaz12 <66207819+Raaz12@users.noreply.github.com>
2022-08-15 01:01:06 +0530

    Time: 4 ms (28.92%), Space: 25.4 MB (26.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,25 +2,16 @@ class Solution {
 public:
     int path(int n,int m,vector<vector<int>>dp)
     {
-        dp[0][0]=1;
-        for(int i=0;i<n;i++)
+        long long int N=n+m-2;
+        m=min(n,m)-1;
+       long long int ans=1;
+        for(int i=1;i<=m;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0 and j==0)   dp[0][0]=1;
-                else
-                {
-                    int up=0;
-                    int down=0;
-                    if(i>0 ) up=dp[i-1][j];
-                    if(j>0)  down=dp[i][j-1];
-                    dp[i][j]=up+down;
-                }
-            }
-            
+            ans=ans*(N)/i;
+            N--;
         }
         
-        return dp[n-1][m-1];
+        return ans;
     }
     int uniquePaths(int n, int m) {
         vector<vector<int>>dp(n*m , vector<int>(n*m,-1));
