a8e4740a724e1c2daef892b7172a83bf290c98f3
Raaz12 <66207819+Raaz12@users.noreply.github.com>
2022-08-15 20:11:51 +0530

    Time: 32 ms (21.15%), Space: 13.2 MB (5.10%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -4,35 +4,56 @@ public:
     {
          for(int i=0;i<7;i++)
             {
+             
                 if(a==v[i].first)
                 {
+                    
                  return i;
+                    
                 }
+             
             }
+        
         return -1;
     }
     int romanToInt(string s) {
+        
         vector<pair<char,int>>v;
+        
         v.push_back(make_pair('I',1));
+        
         v.push_back(make_pair('V',5));
+        
         v.push_back(make_pair('X',10));
+        
         v.push_back(make_pair('L',50));
+        
         v.push_back(make_pair('C',100));
+        
         v.push_back(make_pair('D',500));
+        
         v.push_back(make_pair('M',1000));
+        
         int sum=0;
+        
         int n=s.size();
+        
         int p=0,q=0;
+        
         for(int i=0;i<n;i++)
         {
             if(i<n-1)
             {
                  p=v[pos(s[i],v)].second;
+                
                  q=v[pos(s[i+1],v)].second;
+                
                 if(p<q)
                 {
                     sum+=q-p;
+                    
                     q=0;
+                    
                     i++;
                 }
                 else
@@ -43,7 +64,9 @@ public:
             else
             {
                  p=v[pos(s[i],v)].second;
-                sum+=p;
+                
+                 sum+=p;
+                
             }
         
         }
