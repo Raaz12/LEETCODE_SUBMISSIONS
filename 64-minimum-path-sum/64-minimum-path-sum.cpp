520e6dd01b3a3696a5337e551110b38e158b4800
Raaz12 <66207819+Raaz12@users.noreply.github.com>
2022-08-18 03:33:18 +0530

    Time: 9 ms (87.70%), Space: 10 MB (65.36%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,82 +4,82 @@ class Solution
 public:
 int minPath (int i, int j, vector < vector < int >>&grid,
 		vector < vector < int >>&dp) 
-  {
+    {
     
-if (i == 0 and j == 0)
-      return grid[0][0];
-    
-
-if (i < 0 or j < 0)
-      return 10000;
-    
-if (dp[i][j] != -1)
-      return dp[i][j];
-    
-int up = grid[i][j] + minPath (i - 1, j, grid, dp);
-     
-
-int down = grid[i][j] + minPath (i, j - 1, grid, dp);
-     
-
-return dp[i][j] = min (up, down);
+        if (i == 0 and j == 0)
+              return grid[0][0];
+            
+        
+        if (i < 0 or j < 0)
+              return 10000;
+            
+        if (dp[i][j] != -1)
+              return dp[i][j];
+            
+        int up = grid[i][j] + minPath (i - 1, j, grid, dp);
+             
+        
+        int down = grid[i][j] + minPath (i, j - 1, grid, dp);
+             
+        
+        return dp[i][j] = min (up, down);
+           
+    }
    
-}
-   
-int minPathSum (vector < vector < int >>&grid)
-  {
-    
- 
-int n = grid.size ();
-    
- 
-int m = grid[0].size ();
-    
-vector < vector < int >>dp (n, vector < int >(m, 0));
-    
-for (int i = 0; i < n; i++)
-      
+    int minPathSum (vector < vector < int >>&grid)
       {
-	
-for (int j = 0; j < m; j++)
-	  
-	  {
-	    
-if (i == 0 and j == 0)
-	      dp[i][j] = grid[i][j];
-	    
-	    else
-	      
-	      {
-		
-int up = grid[i][j];
-		
-if (i > 0)
-		  up += dp[i - 1][j];
-		
-		else
-		  up += 1e9;
-		
-int left = grid[i][j];
-		
-if (j > 0)
-		  left += dp[i][j - 1];
-		
-		else
-		  left += 1e9;
-		
-dp[i][j] = min (left, up);
-	      
- 
-}
-	  
-}
-      
-}
-    
-return dp[n - 1][m - 1];
-  
- 
-}
-
+            
+         
+        int n = grid.size ();
+            
+         
+        int m = grid[0].size ();
+            
+        vector < vector < int >>dp (n, vector < int >(m, 0));
+            
+        for (int i = 0; i < n; i++)
+              
+              {
+            
+        for (int j = 0; j < m; j++)
+              
+              {
+                
+        if (i == 0 and j == 0)
+                  dp[i][j] = grid[i][j];
+                
+                else
+                  
+                  {
+                
+        int up = grid[i][j];
+                
+        if (i > 0)
+                  up += dp[i - 1][j];
+                
+                else
+                  up += 1e9;
+                
+        int left = grid[i][j];
+                
+        if (j > 0)
+                  left += dp[i][j - 1];
+                
+                else
+                  left += 1e9;
+                
+        dp[i][j] = min (left, up);
+                  
+         
+        }
+              
+        }
+              
+        }
+            
+        return dp[n - 1][m - 1];
+          
+         
+        }
+        
 };
