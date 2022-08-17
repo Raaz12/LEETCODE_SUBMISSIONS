e947c28c0349c3fa9305a853de5d7b7f468c69e6
Raaz12 <66207819+Raaz12@users.noreply.github.com>
2022-08-18 03:29:57 +0530

    Time: 20 ms (28.37%), Space: 10.1 MB (55.34%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,24 +1,85 @@
-class Solution {
+class Solution
+{
+
 public:
-    int minPath(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp)
-    {
-        if(i==0 and j==0)   return grid[0][0];
-        
-        if(i<0 or j<0)  return 10000;
-        if(dp[i][j]!=-1)    return dp[i][j];
-        int up=grid[i][j]+minPath(i-1,j,grid,dp);
-        
-        int down=grid[i][j]+minPath(i,j-1,grid,dp);
-        
-        return dp[i][j]=min(up,down); 
-    }
-    int minPathSum(vector<vector<int>>& grid) {
-        
-        int i=grid.size();
-        
-        int j=grid[0].size();
-        vector<vector<int>>dp(i,vector<int>(j,-1));
-        return minPath(i-1,j-1,grid,dp);
-        
-    }
+int minPath (int i, int j, vector < vector < int >>&grid,
+		vector < vector < int >>&dp) 
+  {
+    
+if (i == 0 and j == 0)
+      return grid[0][0];
+    
+
+if (i < 0 or j < 0)
+      return 10000;
+    
+if (dp[i][j] != -1)
+      return dp[i][j];
+    
+int up = grid[i][j] + minPath (i - 1, j, grid, dp);
+     
+
+int down = grid[i][j] + minPath (i, j - 1, grid, dp);
+     
+
+return dp[i][j] = min (up, down);
+   
+}
+   
+int minPathSum (vector < vector < int >>&grid)
+  {
+    
+ 
+int n = grid.size ();
+    
+ 
+int m = grid[0].size ();
+    
+vector < vector < int >>dp (n, vector < int >(m, 0));
+    
+for (int i = 0; i < n; i++)
+      
+      {
+	
+for (int j = 0; j < m; j++)
+	  
+	  {
+	    
+if (i == 0 and j == 0)
+	      dp[i][j] = grid[i][j];
+	    
+	    else
+	      
+	      {
+		
+int up = grid[i][j];
+		
+if (i > 0)
+		  up += dp[i - 1][j];
+		
+		else
+		  up += 1e9;
+		
+int left = grid[i][j];
+		
+if (j > 0)
+		  left += dp[i][j - 1];
+		
+		else
+		  left += 1e9;
+		
+dp[i][j] = min (left, up);
+	      
+ 
+}
+	  
+}
+      
+}
+    
+return dp[n - 1][m - 1];
+  
+ 
+}
+
 };
